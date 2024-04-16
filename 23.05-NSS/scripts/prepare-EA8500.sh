4ec87e9c0be7a51a21ace03e84b6fcc89b4bc9c0
Your Name <you@example.com>
2024-04-16 16:27:48 +0800

    23.05-nss

---
+++ 23.05-NSS/scripts/prepare-EA8500.sh
@@ -0,0 +1,10 @@
+cd openwrt
+
+echo "添加包"
+chmod +x ../LeanWRT/scripts/*.sh
+../LeanWRT/scripts/custom_feeds.sh
+
+
+echo "导入编译配置"
+cp -v ../LeanWRT/config/config-EA8500.config .config
+make defconfig
