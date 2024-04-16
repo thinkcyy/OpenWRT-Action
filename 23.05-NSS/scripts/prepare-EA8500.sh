32bbf0d802abf9efcbb9e1d206453370b4e697ed
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-16 18:43:55 +0800

    Update prepare-EA8500.sh

--- 23.05-NSS/scripts/prepare-EA8500.sh
+++ 23.05-NSS/scripts/prepare-EA8500.sh
@@ -1,10 +1,10 @@
 cd openwrt
 
 echo "添加包"
-chmod +x ../LeanWRT/scripts/*.sh
-../LeanWRT/scripts/custom_feeds.sh
+chmod +x ../23.05-NSS/scripts/*.sh
+../23.05-NSS/scripts/custom_feeds.sh
 
 
 echo "导入编译配置"
-cp -v ../LeanWRT/config/config-EA8500.config .config
+cp -v ../23.05-NSS/config/config-EA8500.config .config
 make defconfig
