5d798dac5ad5aa53d16e8b29c13b52024bda61dd
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-16 16:30:40 +0800

    Update custom_feeds.sh

--- 23.05-NSS/scripts/custom_feeds.sh
+++ 23.05-NSS/scripts/custom_feeds.sh
@@ -1,4 +1,30 @@
 # 更新 Feeds
 #cd openwrt
+echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git cus_lean_prouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+
+# 更新 Feeds
+./scripts/feeds update -a
+
+rm -rf feeds/luci/modules/luci-base
+rm -rf feeds/luci/modules/luci-mod-status
+rm -rf feeds/packages/utils/coremark
+rm -rf package/new/default-settings
+
+mkdir package/new
+
+git clone  https://github.com/immortalwrt/packages immortal_package
+cp -r ./immortal_package/utils/coremark package/new/
+
+git clone https://github.com/coolsnowwolf/lede lede
+cp -r ./lede/package/lean feeds/
+
+rm -r feeds/lean/ddns-scripts_aliyun
+rm -r feeds/lean/autocore
+
+
+# ddns-scripts_aliyun
+cp -r ../package/ddns-scripts_aliyun  package/new/
 ./scripts/feeds update -a
 ./scripts/feeds install -a
