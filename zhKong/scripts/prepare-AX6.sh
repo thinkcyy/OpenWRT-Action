5ffdd2246f0196fc407a2b494b43dfa5af9fbae2
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-26 18:38:00 +0800

    Update prepare-AX6.sh

--- zhKong/scripts/prepare-AX6.sh
+++ zhKong/scripts/prepare-AX6.sh
@@ -5,5 +5,5 @@ chmod +x ../zhKong/scripts/*.sh
 ../zhKong/scripts/custom_feeds.sh
 
 # config file
-# cp -v ../zhKong/config/config-AX6.config .config
+cp -v ../zhKong/config/config-AX6.config .config
 make defconfig
