a68a53c031abdcdb1ec241b02244f07b7c50b1c5
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-28 15:02:26 +0800

    Update prepare.sh

--- zhKong/scripts/prepare.sh
+++ zhKong/scripts/prepare.sh
@@ -1,9 +1,9 @@
 cd openwrt
 
-echo "添加软件包"
+echo '当前执行步骤：添加软件包'
 chmod +x ../zhKong/scripts/*.sh
 ../zhKong/scripts/custom_feeds.sh
 
-echo "导入编译配置"
+echo '当前执行步骤：导入编译配置'
 cp -v ../zhKong/config/config-$ROUTER_MODEL.config .config
 make defconfig
