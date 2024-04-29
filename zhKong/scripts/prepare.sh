e9242746e70808925574ba007cd87ff0381ef06c
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-29 10:47:15 +0800

    Update prepare.sh

--- zhKong/scripts/prepare.sh
+++ zhKong/scripts/prepare.sh
@@ -6,4 +6,8 @@ chmod +x ../zhKong/scripts/*.sh
 
 echo '当前执行步骤：导入编译配置'
 cp -v ../zhKong/config/config-$ROUTER_MODEL.config .config
+
+#Action使用
+cp -v ../zhKong/config/config-$INPUT_ROUTER_MODEL.config .config
+
 make defconfig
