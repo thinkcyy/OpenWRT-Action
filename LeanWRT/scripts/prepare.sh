d64a135e1a18b8222b88b4195ceaf1592c02d5bd
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-05-05 15:40:33 +0800

    Create prepare.sh

---
+++ LeanWRT/scripts/prepare.sh
@@ -0,0 +1,14 @@
+cd openwrt
+
+echo '当前执行步骤：2.1-执行custom_feeds'
+chmod +x ../LeanWRT/scripts/*.sh
+../LeanWRT/scripts/custom_feeds.sh
+
+echo '当前执行步骤：2.2-导入编译配置'
+cp -v ../LeanWRT/config/config-$ROUTER_MODEL.config .config
+
+#Action使用
+cp -v ../LeanWRT/config/config-$INPUT_ROUTER_MODEL.config .config
+
+echo '当前执行步骤：2.3-初始化编译配置defconfig'
+make defconfig
