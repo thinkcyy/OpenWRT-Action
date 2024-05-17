a5c50fcb3c81fd4f73fa624727b6b6edbc0f678b
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-05-18 03:33:21 +0800

    Update prepare.sh

--- LeanWRT/scripts/prepare.sh
+++ LeanWRT/scripts/prepare.sh
@@ -5,11 +5,13 @@ chmod +x ../LeanWRT/scripts/*.sh
 ../LeanWRT/scripts/custom_feeds.sh
 
 echo '当前执行步骤：2.2-导入编译配置'
+echo 'ROUTER_MODEL为： ${ROUTER_MODEL}'
+echo 'INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}'
 cp -v ../LeanWRT/config/config-$ROUTER_MODEL.config .config
 
 #Action使用
 cp -v ../LeanWRT/config/config-$INPUT_ROUTER_MODEL.config .config
-cp -v ../LeanWRT/config/config-${{ inputs.ROUTER_MODEL }}.config .config
+#cp -v ../LeanWRT/config/config-${{ inputs.ROUTER_MODEL }}.config .config
 
 echo '当前执行步骤：2.3-初始化编译配置defconfig'
 make defconfig
