d37aea317f8d8d7ec37fe1479aef6901d4443071
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-05-24 18:44:13 +0800

    Update prepare.sh

--- LeanWRT/scripts/prepare.sh
+++ LeanWRT/scripts/prepare.sh
@@ -1,18 +1,18 @@
 cd openwrt
 
 echo '当前执行步骤：2.1-执行custom_feeds'
-chmod +x ../LeanWRT/scripts/*.sh
-../LeanWRT/scripts/custom_feeds.sh
+chmod +x ../OpenWrt/scripts/*.sh
+../OpenWrt/scripts/custom_feeds.sh
 
 echo '当前执行步骤：2.2-导入编译配置'
 echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
 echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
 #echo {{ inputs.ROUTER_MODEL }}为：${{inputs.ROUTER_MODEL}}
-cp -v ../LeanWRT/config/config-${ROUTER_MODEL}.config .config
+cp -v ../OpenWrt/config/config-${ROUTER_MODEL}.config .config
 
 #Action使用
-cp -v ../LeanWRT/config/config-${INPUT_ROUTER_MODEL}.config .config
-#cp -v ../LeanWRT/config/config-${{ inputs.ROUTER_MODEL }}.config .config
+cp -v ../OpenWrt/config/config-${INPUT_ROUTER_MODEL}.config .config
+#cp -v ../OpenWrt/config/config-${{ inputs.ROUTER_MODEL }}.config .config
 
 echo '当前执行步骤：2.3-初始化编译配置defconfig'
 make defconfig
