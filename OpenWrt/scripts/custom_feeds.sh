74eda999679ab39f0171aaaa2cbcc95c7369264e
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-05-27 14:01:26 +0800

    Update custom_feeds.sh

--- OpenWrt/scripts/custom_feeds.sh
+++ OpenWrt/scripts/custom_feeds.sh
@@ -1,21 +1,21 @@
 # 更新 Feeds
 #cd openwrt
-#echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-#echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-#echo 'src-git cus_lean_prouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git cus_lean_prouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 
 # 更新 Feeds
 ./scripts/feeds update -a
 
-rm -rf feeds/luci/modules/luci-base
-rm -rf feeds/luci/modules/luci-mod-status
-rm -rf feeds/packages/utils/coremark
-rm -rf package/new/default-settings
+#rm -rf feeds/luci/modules/luci-base
+#rm -rf feeds/luci/modules/luci-mod-status
+#rm -rf feeds/packages/utils/coremark
+#rm -rf package/new/default-settings
 
 mkdir package/new
 
-git clone  https://github.com/immortalwrt/packages immortal_package
-cp -r ./immortal_package/utils/coremark package/new/
+#git clone  https://github.com/immortalwrt/packages immortal_package
+#cp -r ./immortal_package/utils/coremark package/new/
 
 git clone https://github.com/coolsnowwolf/lede lede
 cp -r ./lede/package/lean feeds/
