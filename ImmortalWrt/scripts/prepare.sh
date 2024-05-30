ff274b797497b14a1490ec81cd9f7da5717f6c23
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-05-30 15:31:33 +0800

    Update prepare.sh

--- ImmortalWrt/scripts/prepare.sh
+++ ImmortalWrt/scripts/prepare.sh
@@ -1,20 +1,20 @@
-cd openwrt
+cd ImmortalWrt
 
 echo '当前执行步骤：2.1-执行custom_feeds'
-chmod +x ../OpenWrt/scripts/*.sh
+chmod +x ../ImmortalWrt/scripts/*.sh
 echo "custom_feeds内容"
-cat ../OpenWrt/scripts/custom_feeds.sh
-../OpenWrt/scripts/custom_feeds.sh
+cat ../ImmortalWrt/scripts/custom_feeds.sh
+../ImmortalWrt/scripts/custom_feeds.sh
 
 echo '当前执行步骤：2.2-导入编译配置'
 echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
 echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
 #echo {{ inputs.ROUTER_MODEL }}为：${{inputs.ROUTER_MODEL}}
-cp -v ../OpenWrt/config/config-${ROUTER_MODEL}.config .config
+cp -v ../ImmortalWrt/config/config-${ROUTER_MODEL}.config .config
 
 #Action使用
-cp -v ../OpenWrt/config/config-${INPUT_ROUTER_MODEL}.config .config
-#cp -v ../OpenWrt/config/config-${{ inputs.ROUTER_MODEL }}.config .config
+cp -v ../ImmortalWrt/config/config-${INPUT_ROUTER_MODEL}.config .config
+#cp -v ../ImmortalWrt/config/config-${{ inputs.ROUTER_MODEL }}.config .config
 
 echo '当前执行步骤：2.3-初始化编译配置defconfig'
 make defconfig
