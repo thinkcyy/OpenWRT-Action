f3900d4f64b8ab2f783e766452fbd56b0cbcd202
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-06-27 12:45:34 +0800

    Update custom_feeds.sh

--- zhKong/scripts/custom_feeds.sh
+++ zhKong/scripts/custom_feeds.sh
@@ -1,7 +1,7 @@
-echo '-更新 Feeds'
+echo '-步骤：custom_feed-更新 Feeds'
 ./scripts/feeds update -a
 
-echo '-替换自带feed中的luci-base、luci-mod-status、coremark和自带package中的default-settings'
+echo '-步骤：custom_feed-替换自带feed中的luci-base、luci-mod-status、coremark和自带package中的default-settings'
 mkdir package/immortal
 
 git clone https://github.com/immortalwrt/luci immortal_luci
@@ -34,11 +34,11 @@ cp -r ./immortal_luci/modules/luci-mod-status feeds/luci/modules/
 cp -r ./immortal_package/utils/coremark package/immortal/
 cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
 
-echo '-添加autocore'
+echo '-步骤：custom_feed-添加autocore'
 cp -r ./immortal_immortalwrt/package/emortal/autocore package/immortal/
 sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/immortal/autocore/files/luci-mod-status-autocore.json
 
-#echo '-添加lean自带的lean软件目录'
+#echo '-步骤：custom_feed-添加lean自带的lean软件目录'
 #git clone --depth 1 https://github.com/coolsnowwolf/lede lede
 #cp -r ./lede/package/lean feeds/
 # 删除lean的ddns-scripts_aliyun、dedefault-settings
@@ -48,22 +48,22 @@ sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package
 
 ./scripts/feeds install -a
 
-echo '-添加zhKong的ddns-scripts_aliyun包'
+echo '-步骤：custom_feed-添加zhKong的ddns-scripts_aliyun包'
 git clone --depth 1 https://github.com/thinkcyy/AX3600-OpenWrt zhKong_OpenWrt
 cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/immortal/
 #tree -L 3 ./package
 
-echo '-向后调整tinc服务启动次序'             
+echo '-步骤：custom_feed-向后调整tinc服务启动次序'             
 sed -i 's|START=42|START=99|g' ./feeds/packages/net/tinc/files/tinc.init
 
 echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
 echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
 
-echo '-导入编译配置'
+echo '-步骤：custom_feed-导入编译配置'
 cp -v ../zhKong/config/config-${ROUTER_MODEL}.config .config
 
-echo '-Action导入编译配置'
+echo '-步骤：custom_feed-Action导入编译配置'
 cp -v ../zhKong/config/config-${INPUT_ROUTER_MODEL}.config .config
 
-echo '-初始化编译配置defconfig'
+echo '-步骤：custom_feed-初始化编译配置defconfig'
 make defconfig
