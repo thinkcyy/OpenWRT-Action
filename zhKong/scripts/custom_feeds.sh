546887481b6bcb28cc23396d73f08f593d148e3b
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-06-27 10:40:43 +0800

    Update custom_feeds.sh

--- zhKong/scripts/custom_feeds.sh
+++ zhKong/scripts/custom_feeds.sh
@@ -1,16 +1,27 @@
-echo '当前执行步骤：2.1.1-custom_feeds-添加lean的LUCI、packages、和routing的feed源'
-#echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-#echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-#echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-
-echo '当前执行步骤：2.1.2-custom_feeds-更新 Feeds'
+echo '-更新 Feeds'
 ./scripts/feeds update -a
 
-echo '当前执行步骤：2.1.3-custom_feeds-替换自带feed中的luci-base、luci-mod-status、coremark和自带package中的default-settings'
+echo '-替换自带feed中的luci-base、luci-mod-status、coremark和自带package中的default-settings'
 mkdir package/immortal
-git clone --depth 1 https://github.com/immortalwrt/luci immortal_luci
-git clone --depth 1 https://github.com/immortalwrt/packages immortal_package
-git clone --depth 1 https://github.com/immortalwrt/immortalwrt immortal_immortalwrt
+
+git clone https://github.com/immortalwrt/luci immortal_luci
+git clone https://github.com/immortalwrt/packages immortal_package
+git clone https://github.com/immortalwrt/immortalwrt immortal_immortalwrt
+
+# 锁定日期
+REV_DATE=$(git log -1 --format=%cd --date=iso8601-strict)
+
+cd immortal_luci
+REV_HASH="$(git rev-list -n 1 --all --before=${REV_DATE}"
+git checkout $REV_HASH
+
+cd ../immortal_package
+REV_HASH="$(git rev-list -n 1 --all --before=${REV_DATE}"
+git checkout $REV_HASH
+
+cd ../immortal_immortalwrt
+REV_HASH="$(git rev-list -n 1 --all --before=${REV_DATE}"
+git checkout $REV_HASH
 
 rm -rf feeds/luci/modules/luci-base
 rm -rf feeds/luci/modules/luci-mod-status
@@ -21,11 +32,11 @@ cp -r ./immortal_luci/modules/luci-mod-status feeds/luci/modules/
 cp -r ./immortal_package/utils/coremark package/immortal/
 cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
 
-#添加autocore
+echo '-添加autocore'
 cp -r ./immortal_immortalwrt/package/emortal/autocore package/immortal/
 sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/immortal/autocore/files/luci-mod-status-autocore.json
 
-#echo '当前执行步骤：2.1.4-custom_feeds-添加lean自带的lean软件目录'
+#echo '-添加lean自带的lean软件目录'
 #git clone --depth 1 https://github.com/coolsnowwolf/lede lede
 #cp -r ./lede/package/lean feeds/
 # 删除lean的ddns-scripts_aliyun、dedefault-settings
@@ -33,10 +44,9 @@ sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package
 #rm -r feeds/lean/autocore
 #rm -r feeds/lean/default-settings
 
-
 ./scripts/feeds install -a
 
-echo '当前执行步骤：2.1.5-custom_feeds-添加zhKong的ddns-scripts_aliyun包'
+echo '-添加zhKong的ddns-scripts_aliyun包'
 git clone --depth 1 https://github.com/thinkcyy/AX3600-OpenWrt zhKong_OpenWrt
 cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/immortal/
 tree -L 3 ./package
