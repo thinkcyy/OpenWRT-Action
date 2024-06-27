7be08c08f9967470adad1364310c2216e21989c4
Your Name <you@example.com>
2024-06-27 10:01:58 +0800

    select_feed_commit.sh

---
+++ scripts/select_feed_commit.sh
@@ -0,0 +1,11 @@
+sed -e "/^src-git\S*/s//src-git-full/" feeds.conf.default > feeds.conf
+sed -i "/^\#/d" feeds.conf
+./scripts/feeds update -a
+sed -e "/^src-git\S*\s/{s///;s/\s.*$//p}" feeds.conf  | while read -r FEED_ID
+do
+REV_DATE=$(git log -1 --format=%cd --date=iso8601-strict)
+#echo $REV_DATE
+REV_HASH=$(git -C feeds/${FEED_ID} rev-list -n 1 --all --before=${REV_DATE})
+echo $REV_HASH
+sed -i -e "/\s${FEED_ID}\s.*\.git$/s/$/^${REV_HASH}/" feeds.conf
+done
