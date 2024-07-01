67a10caf8830292dd1f1e34b9726867c1e45fbf5
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-01 15:25:46 +0800

    Update custom_feeds-202403.sh

--- zhKong/scripts/custom_feeds-202403.sh
+++ zhKong/scripts/custom_feeds-202403.sh
@@ -1,6 +1,6 @@
-echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+#echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+#echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+#echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 
 # 更新 Feeds
 ./scripts/feeds update -a
