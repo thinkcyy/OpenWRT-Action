9576d789eaf55306785d08478cc763fcdb5559cb
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-03 14:08:33 +0800

    Update add_feeds_cus_lean.sh

--- zhKong/scripts/add_feeds_cus_lean.sh
+++ zhKong/scripts/add_feeds_cus_lean.sh
@@ -1,5 +1,5 @@
 #echo '-添加额外feed源'
-echo '-cus_lean源'
+echo '-添加lean的luci、package和routing源'
 echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci.git' >>feeds.conf.default
 echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages.git' >>feeds.conf.default
 echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing.git' >>feeds.conf.default
