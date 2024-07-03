f83a95635b4a6dfc3088ade8a99086561538ad18
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-03 17:24:53 +0800

    Update custom_feeds-202403.sh

--- zhKong/scripts/custom_feeds-202403.sh
+++ zhKong/scripts/custom_feeds-202403.sh
@@ -24,27 +24,27 @@ cd ../lede
 
 cd ..
 
-#echo '-步骤：custom_feed-替换自带luci-base、luci-mod-status'
-#rm -rf feeds/luci/modules/luci-base
-#rm -rf feeds/luci/modules/luci-mod-status
-#cp -r ./immortal_luci/modules/luci-base feeds/luci/modules/
-#cp -r ./immortal_luci/modules/luci-mod-status feeds/luci/modules/
+echo '-步骤：custom_feed-替换自带luci-base、luci-mod-status'
+rm -rf feeds/luci/modules/luci-base
+rm -rf feeds/luci/modules/luci-mod-status
+cp -r ./immortal_luci/modules/luci-base feeds/luci/modules/
+cp -r ./immortal_luci/modules/luci-mod-status feeds/luci/modules/
 
-#echo '-步骤：custom_feed-替换自带coremark'
-#rm -rf feeds/packages/utils/coremark
-#cp -r ./immortal_package/utils/coremark package/immortal
+echo '-步骤：custom_feed-替换自带coremark'
+rm -rf feeds/packages/utils/coremark
+cp -r ./immortal_package/utils/coremark package/immortal
 
-#echo '-步骤：custom_feed-添加autocore'
-#cp -r ./immortal_immortalwrt/package/emortal/autocore package/immortal/
-#sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/immortal/autocore/files/luci-mod-status-autocore.json
+echo '-步骤：custom_feed-添加autocore'
+cp -r ./immortal_immortalwrt/package/emortal/autocore package/immortal/
+sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/immortal/autocore/files/luci-mod-status-autocore.json
 
-#echo '-步骤：custom_feed-替换自带default-settings'
-#cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
+echo '-步骤：custom_feed-替换自带default-settings'
+cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
 
-#echo '-步骤：custom_feed-添加lean自带的lean软件目录'
-#cp -r ./lede/package/lean package/
-#rm -rf package/lean/ddns-scripts_aliyun
-#rm -rf package/lean/autocore
+echo '-步骤：custom_feed-添加lean自带的lean软件目录'
+cp -r ./lede/package/lean package/
+rm -rf package/lean/ddns-scripts_aliyun
+rm -rf package/lean/autocore
 #rm -rf package/lean/default-settings
  
 ./scripts/feeds install -a
@@ -55,3 +55,15 @@ cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/immortal
 
 echo '-步骤：custom_feed-向后调整tinc服务启动次序'             
 sed -i 's|START=42|START=99|g' ./feeds/packages/net/tinc/files/tinc.init
+
+echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
+echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
+
+echo '-步骤：custom_feed-导入编译配置'
+cp -v ../zhKong/config/config-${ROUTER_MODEL}.config .config
+
+echo '-步骤：custom_feed-Action导入编译配置'
+cp -v ../zhKong/config/config-${INPUT_ROUTER_MODEL}.config .config
+
+echo '-步骤：custom_feed-初始化编译配置defconfig'
+make defconfig
