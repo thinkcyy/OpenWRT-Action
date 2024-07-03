a0a41220e4f2ee46c275141a5c864e321c23624a
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-03 17:34:24 +0800

    Update custom_feeds-202403.sh

--- zhKong/scripts/custom_feeds-202403.sh
+++ zhKong/scripts/custom_feeds-202403.sh
@@ -62,8 +62,5 @@ echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
 echo '-步骤：custom_feed-导入编译配置'
 cp -v ../zhKong/config/config-${ROUTER_MODEL}.config .config
 
-echo '-步骤：custom_feed-Action导入编译配置'
-cp -v ../zhKong/config/config-${INPUT_ROUTER_MODEL}.config .config
-
 echo '-步骤：custom_feed-初始化编译配置defconfig'
 make defconfig
