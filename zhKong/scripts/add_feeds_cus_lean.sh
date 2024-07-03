872551ca4e308246ec2525877e9805ec3c3711ef
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-03 11:43:14 +0800

    Create add_feeds_cus_lean.sh

---
+++ zhKong/scripts/add_feeds_cus_lean.sh
@@ -0,0 +1,5 @@
+echo '-添加额外feed源'
+echo '-cus_lean源'
+echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
