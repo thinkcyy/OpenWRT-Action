fb840791a8af1e1773fb3c0f5e9885dd2f016ab3
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-03 11:20:08 +0800

    Update custom_feeds-202403.sh

--- zhKong/scripts/custom_feeds-202403.sh
+++ zhKong/scripts/custom_feeds-202403.sh
@@ -1,7 +1,3 @@
-echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-
 # 更新 Feeds
 echo '当前工作目录'
 pwd
