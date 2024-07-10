84919915266e5200cc5701d05b9159df056cc135
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-07-11 01:12:45 +0800

    Update custom_feeds-AgustinLorenzo.sh

--- zhKong/scripts/custom_feeds-AgustinLorenzo.sh
+++ zhKong/scripts/custom_feeds-AgustinLorenzo.sh
@@ -9,6 +9,7 @@ git clone https://github.com/immortalwrt/packages immortal_package
 git clone https://github.com/immortalwrt/immortalwrt immortal_immortalwrt
 git clone https://github.com/coolsnowwolf/lede lede
 
+: <<'COMMENT'
 # 锁定日期
 cd immortal_luci
 ../../scripts/select_dir_commit.sh
@@ -23,6 +24,8 @@ cd ../lede
 ../../scripts/select_dir_commit.sh
 
 cd ..
+COMMENT
+
 
 echo '-步骤：custom_feed-替换自带luci-base、luci-mod-status'
 rm -rf feeds/luci/modules/luci-base
@@ -45,19 +48,34 @@ echo '-步骤：custom_feed-添加lean自带的lean软件目录'
 cp -r ./lede/package/lean package/
 rm -rf package/lean/ddns-scripts_aliyun
 rm -rf package/lean/autocore
-#rm -rf package/lean/default-settings
+rm -rf package/lean/default-settings
 
 mkdir -p ./package/thinkcy
-cp -vr ../thinkcy-settings ./package/thinkcy 
-
-./scripts/feeds install -a
+cp -r ../thinkcy-settings ./package/thinkcy 
 
 echo '-步骤：custom_feed-添加zhKong的ddns-scripts_aliyun包'
 git clone --depth 1 https://github.com/thinkcyy/AX3600-OpenWrt  zhKong_OpenWrt
-cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/immortal
+cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/thinkcy/
+
+echo '-步骤：custom_feed-添加lean的luci仓库'
+git clone https://github.com/coolsnowwolf/luci cus_lean_luci
+cp -r ./cus_lean_luci/applications/luci-app-turboacc package/thinkcy/
+
+
+echo '-步骤：custom_feed-修改默认语言'
+sed -i "s|option lang auto|option lang \'zh_cn\'|g" ./feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i '/config internal languages/a \ \ \ \ \ \ \ \ option zh_cn chinese' ./feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i '/config internal languages/a \ \ \ \ \ \ \ \ option en English' ./feeds/luci/modules/luci-base/root/etc/config/luci
+
+
+echo '-步骤：custom_feed-添加zhKong的ddns-scripts_aliyun包'
+git clone --depth 1 https://github.com/thinkcyy/AX3600-OpenWrt  zhKong_OpenWrt
+cp -vr ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/thinkcy/
 
 echo '-步骤：custom_feed-向后调整tinc服务启动次序'             
 sed -i 's|START=42|START=99|g' ./feeds/packages/net/tinc/files/tinc.init
+
+./scripts/feeds install -a
 
 echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
 echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
