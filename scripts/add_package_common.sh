fddb605e223d54fe7b34975d4b5eec6cbc8a0002
Your Name <you@example.com>
2024-07-23 18:51:51 +0800

    add_package_common.sh

---
+++ scripts/add_package_common.sh
@@ -0,0 +1,9 @@
+echo "-当前执行步骤：锁定已下载feeds目录日期"
+
+echo ${REV_DATE}
+FEED_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo ${FEED_BRANCH}
+REV_HASH=$(git rev-list -n 1 --before=${REV_DATE} ${FEED_BRANCH})
+PWD=$(pwd)
+echo --拟切换$PWD至HASH：$REV_HASH
+git checkout $REV_HASH
