ecda61b1aea1cdd648d083dc193b300365038af8
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-08-09 16:46:52 +0800

    Update custom_feeds-Lean.sh

--- LeanWRT/scripts/custom_feeds-Lean.sh
+++ LeanWRT/scripts/custom_feeds-Lean.sh
@@ -13,10 +13,10 @@ sed -i 's|START=42|START=99|g' ./feeds/packages/net/tinc/files/tinc.init
 ./scripts/feeds install -a
 
 echo "ROUTER_MODEL为： ${ROUTER_MODEL}"
-echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
+#echo "INPUT_ROUTER_MODEL为： ${INPUT_ROUTER_MODEL}"
 
 echo '-步骤：custom_feed-导入编译配置'
-cp -v ../zhKong/config/config-${ROUTER_MODEL}.config .config
+cp -v ../${REPO_TYPE}/config/config-${ROUTER_MODEL}.config .config
 
 echo '-步骤：custom_feed-初始化编译配置defconfig'
 make defconfig
