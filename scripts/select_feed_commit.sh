72c8d26b5e6fc62246b3402cfa3a3fdc8d89787c
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-10-17 14:29:42 +0800

    Update select_feed_commit.sh

--- scripts/select_feed_commit.sh
+++ scripts/select_feed_commit.sh
@@ -4,14 +4,26 @@ lock_date() {
         REV_HASH=$(git rev-list -n 1 --before=${REV_DATE} ${FEED_BRANCH})
         echo $FEED_ID对应的HASH为：$REV_HASH
         cd ../..
-        sed  -i -e "/\s${FEED_ID}\s.*\.git$/s/$/^${REV_HASH}/" feeds-temp.conf 
-        sed -n '/${FEED_ID}/p' feeds-temp.conf >> feeds.conf
+        sed  -i -e "/\s${FEED_ID}\s.*\.git$/s/$/^${REV_HASH}/" feeds.conf 
+        if [  -n "$2" ] ;then
+                sed -n '/${FEED_ID}/p' feeds.conf >> feeds-locked.conf
+                cp feeds-locked.conf feeds.conf
+        fi
 }
 
-echo "-当前执行步骤：锁定feeds日期"
-sed -e "/^src-git\S*/s//src-git-full/" feeds.conf.default > feeds-temp.conf
-sed -i "/^\#/d" feeds-temp.conf
-#./scripts/feeds update -a
+sed -e "/^src-git\S*/s//src-git-full/" feeds.conf.default > feeds.conf        
+sed -i "/^\#/d" feeds.conf
+
+if [  -n "$2" ] ;then
+        echo "--当前执行步骤：锁定$2日期"
+        cp feeds.conf feeds-locked.conf
+        sed -i -e "/${FEED_ID}/d' feeds-locked.conf        
+else
+        echo "-当前执行步骤：锁定feeds日期"
+        sed -e "/^src-git\S*/s//src-git-full/" feeds.conf.default > feeds.conf        
+        sed -i "/^\#/d" feeds.conf
+        ./scripts/feeds update -a
+fi
 
 if [  -n "$1" ] ;then
   REV_DATE=$1
@@ -21,12 +33,12 @@ fi
 echo "选定的日期为：$REV_DATE"
 
 #删除分支标记
-sed -i -e "s/\;.*$//g" feeds-temp.conf
+sed -i -e "s/\;.*$//g" feeds.conf
 #添加.git后缀
-sed -i "s/\.git//g" feeds-temp.conf
-sed -i "s/$/\.git/g" feeds-temp.conf
+sed -i "s/\.git//g" feeds.conf
+sed -i "s/$/\.git/g" feeds.conf
 
-sed -e "/^src-git\S*\s/{s///;s/\s.*$//p}" feeds-temp.conf  | while read -r FEED_ID
+sed -e "/^src-git\S*\s/{s///;s/\s.*$//p}" feeds.conf  | while read -r FEED_ID
 do
   if [  -n "$2" ] ;then
     if [  "$FEED_ID" = "$2" ] ;then  
