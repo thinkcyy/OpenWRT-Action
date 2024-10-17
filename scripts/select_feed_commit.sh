3fb2ce61310b2cf05fb01451d8b5c4201dbdc274
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-10-17 15:55:00 +0800

    Update select_feed_commit.sh

--- scripts/select_feed_commit.sh
+++ scripts/select_feed_commit.sh
@@ -3,10 +3,12 @@ lock_date() {
         FEED_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
         REV_HASH=$(git rev-list -n 1 --before=${REV_DATE} ${FEED_BRANCH})
         echo $FEED_ID对应的HASH为：$REV_HASH
+        cd ../..
         sed -i -e "/\s${FEED_ID}\s.*\.git$/s/$/^${REV_HASH}/" feeds.conf
         echo feeds.conf内容为：
-        cat feeds.conf
-        cd ../..        
+        cat feeds.conf                
+        
+        #单独设定某仓库hash时，在上一轮打标成果基础上添加该仓库hash
         if [  -n "$2" ] ;then
                 sed -n '/${FEED_ID}/p' feeds.conf >> feeds-locked.conf
                 cp feeds-locked.conf feeds.conf
