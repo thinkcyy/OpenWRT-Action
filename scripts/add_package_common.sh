d01539004903a3ae045258602f7957675527749c
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-11-21 12:17:31 +0800

    Update add_package_common.sh

--- scripts/add_package_common.sh
+++ scripts/add_package_common.sh
@@ -1,5 +1,6 @@
 echo "-当前执行步骤：添加通用软件包"
 
 [[ -d "package/thinkcy" ]] && mkdir -p package/thinkcy
-git clone https://github.com/izilzty/luci-app-temp-status ./package/thinkcy/luci-app-temp-status
-cp -vr ../thinkcy-settings ./package/thinkcy/ 
+# git clone https://github.com/izilzty/luci-app-temp-status ./package/thinkcy/luci-app-temp-status
+cp -r ../thinkcy ./package/
+cp -r ../thinkcy-settings ./package/thinkcy/ 
