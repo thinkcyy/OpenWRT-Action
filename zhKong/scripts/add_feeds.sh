26949d59d4c5676383ca223d593953e0f54cc133
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-03-02 18:55:10 +0800

    Update add_feeds.sh

--- zhKong/scripts/add_feeds.sh
+++ zhKong/scripts/add_feeds.sh
@@ -1,4 +1,4 @@
 echo '-添加额外feed源'
 echo "src-git thinkcy_qosmio https://github.com/qosmio/packages-extra" | cat - feeds.conf.default                                                     > feeds.conf.add
-cp feeds.conf.add feeds.conf.last ; echo "src-git thinkcy_mwarning https://github.com/mwarning/zerotier-openwrt.git" | cat - feeds.conf.add.last      >> feeds.conf.add
+cp feeds.conf.add feeds.conf.last ; echo "src-git thinkcy_mwarning https://github.com/mwarning/zerotier-openwrt.git" | cat - feeds.conf.last          >> feeds.conf.add
 cp feeds.conf.add feeds.conf.default
