8f318c1c36451184f13b673daa3b18904e46b9db
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-03-02 18:36:02 +0800

    Update add_feeds.sh

--- zhKong/scripts/add_feeds.sh
+++ zhKong/scripts/add_feeds.sh
@@ -1,4 +1,4 @@
 echo '-添加额外feed源'
-#echo 'src-git cus_lean_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-#echo 'src-git cus_lean_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-#echo 'src-git cus_lean_routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo "src-git thinkcy_qosmio https://github.com/qosmio/packages-extra" | cat - feeds.conf.default       > feeds.conf.add
+echo "src-git thinkcy_mwarning https://github.com/mwarning/zerotier-openwrt.git" | cat -                >> feeds.conf.add
+cp feeds.conf.add feeds.conf.default
