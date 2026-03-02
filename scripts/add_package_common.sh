4834fffb8fdd52151cfd9896a9123592804b5120
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-03-02 15:58:53 +0800

    Update add_package_common.sh

--- scripts/add_package_common.sh
+++ scripts/add_package_common.sh
@@ -3,7 +3,7 @@ echo "-当前执行步骤：添加通用软件包"
 [[ -d "package/thinkcy" ]] && mkdir -p package/thinkcy
 # git clone https://github.com/izilzty/luci-app-temp-status ./package/thinkcy/luci-app-temp-status
 cp -r ../thinkcy ./package/
-cp -r ../thinkcy-settings ./package/thinkcy/ 
+
 
 echo "src-git qosmioextra https://github.com/qosmio/packages-extra" >> feeds.conf
 echo "src-git zerotier https://github.com/mwarning/zerotier-openwrt.git" >> feeds.conf
