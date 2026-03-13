523eff2afa982734cc699175470472bd15da53e3
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-03-13 16:44:08 +0800

    Update add_feeds.sh

--- Qosmio/scripts/add_feeds.sh
+++ Qosmio/scripts/add_feeds.sh
@@ -1,4 +1,2 @@
-echo '-添加额外feed源'
-echo "src-git thinkcy_qosmio https://github.com/qosmio/packages-extra" | cat - feeds.conf.default                > feeds.conf.add
-echo "src-git thinkcy_mwarning https://github.com/mwarning/zerotier-openwrt.git" | cat - feeds.conf.default      >> feeds.conf.add
-cp feeds.conf.add feeds.conf.default
+echo '-添加仓库特定feed源：Qosmio-scripts-add_feeds.sh'
+
