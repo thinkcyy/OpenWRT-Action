979a3253e923ae4c84ada39c45258d1d2f0cf72c
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-08-10 19:11:08 +0800

    Update custom_feeds-tingalvin.sh

--- zhKong/scripts/custom_feeds-tingalvin.sh
+++ zhKong/scripts/custom_feeds-tingalvin.sh
@@ -1,50 +1,17 @@
-# for AgustinLorenzo/openwrt:main_nss
+echo "-步骤：custom_feeds自定义软件包-0-$(basename "$0")"
 
-echo '-步骤：custom_feed-add_package_common.sh：添加自定义软件源'
-../scripts/add_package_common.sh
+# 1-调整订阅-1.1-通用软件源
+../scripts/add_package_part1_before_feeds_update-add_feeds.sh
 
-echo '-步骤：custom_feed-feeds update：下载软件源索引信息'
+# 1-调整订阅-1.2-特定软件源
+
+echo '--当前执行步骤：custom_feeds自定义软件包-1-调整订阅-1.3-更新软件源索引信息'
 ./scripts/feeds update -a
 
-echo '-步骤：custom_feed-替换自带feed中的luci-base、luci-mod-status、coremark和自带package中的default-settings'
-mkdir package/immortal
+# 2-调整源码-2.1-通用软件源码
+../scripts/add_package_part2_after_feeds_update-modify_sources.sh
 
-git clone https://github.com/immortalwrt/luci immortal_luci
-git clone https://github.com/immortalwrt/packages immortal_package
-git clone https://github.com/immortalwrt/immortalwrt immortal_immortalwrt
-git clone https://github.com/coolsnowwolf/lede lede
-
-echo '-步骤：custom_feed-替换自带luci-base、luci-mod-status'
-rm -rf feeds/luci/modules/luci-base
-rm -rf feeds/luci/modules/luci-mod-status
-cp -r ./immortal_luci/modules/luci-base feeds/luci/modules/
-cp -r ./immortal_luci/modules/luci-mod-status feeds/luci/modules/
-
-# 来自https://github.com/tingalvin/r7800
-echo '-步骤：enable VHT mode on 2.4g and show NSS load in status'
-cp -r ../thinkcy/patch/feeds/ ./
-echo 当前文件
-tree  feeds/luci/modules/luci-mod-network
-tree feeds/luci/modules/luci-mod-status
-
-echo '-步骤：custom_feed-替换自带default-settings'
-cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
-
-echo '-步骤：custom_feed-添加lean的luci仓库'
-git clone https://github.com/coolsnowwolf/luci cus_lean_luci
-cp -r ./cus_lean_luci/applications/luci-app-turboacc package/thinkcy/
-
-echo '-步骤：custom_feed-修改默认语言'
-sed -i "s|option lang auto|option lang \'zh_cn\'|g" ./feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/config internal languages/a \ \ \ \ \ \ \ \ option en English' ./feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/config internal languages/a \ \ \ \ \ \ \ \ option zh_cn chinese' ./feeds/luci/modules/luci-base/root/etc/config/luci
-
-echo '-步骤：custom_feed-添加zhKong的ddns-scripts_aliyun包'
-git clone --depth 1 https://github.com/thinkcyy/AX3600-OpenWrt  zhKong_OpenWrt
-cp -r ./zhKong_OpenWrt/package/ddns-scripts_aliyun  package/thinkcy/
-
-echo '-步骤：custom_feed-向后调整tinc服务启动次序'             
-sed -i 's|START=42|START=99|g' ./feeds/packages/net/tinc/files/tinc.init
+# 2-调整源码-2.2-特定软件源码
 
 ./scripts/feeds install -a
 
