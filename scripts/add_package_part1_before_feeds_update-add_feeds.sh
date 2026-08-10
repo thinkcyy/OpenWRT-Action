bf8ad03661d230ba1122e3dbe6f9c912bbe19ea5
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-08-10 17:51:21 +0800

    Update add_package_part1_before_feeds_update-add_feeds.sh

--- scripts/add_package_part1_before_feeds_update-add_feeds.sh
+++ scripts/add_package_part1_before_feeds_update-add_feeds.sh
@@ -1,4 +1,4 @@
-echo "--当前执行步骤：custom_feeds自定义软件包-1-通用软件包-1.1-调整订阅$(basename "$0")"
+echo "--当前执行步骤：custom_feeds自定义软件包-1-调整订阅-1.1-通用软件包-$(basename "$0")"
 
 # luci-mod-status-nss
 echo "src-git thinkcy_qosmio https://github.com/qosmio/packages-extra" | cat - feeds.conf.default                                                     > feeds.conf.add
