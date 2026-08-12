00357faf48640a429068467f975a27a57956e563
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-08-12 23:36:29 +0800

    Update add_package_part2_after_feeds_update-modify_sources.sh

--- scripts/add_package_part2_after_feeds_update-modify_sources.sh
+++ scripts/add_package_part2_after_feeds_update-modify_sources.sh
@@ -26,14 +26,10 @@ cp -r ./immortal_immortalwrt/package/emortal/default-settings package/immortal/
 #cp -r ./cus_lean_luci/applications/luci-app-turboacc package/thinkcy/
 
 echo '---当前执行步骤：custom_feeds自定义软件包-2-调整源码-2.1-通用源码-2.1.5-添加自带源码thinkcy_package'
-# 替换feeds同名软件包（tinc、frpc）
+# 替换feeds同名软件包（tinc、frpc、luci-app-frpc）
 cp -r ../thinkcy/package ./
 # 添加thinkcy自带软件包（thinkcy-settings、luci-app-temp-status）
 cp -r ../thinkcy/thinkcy_package ./package/
-# 替换为适配frpc toml格式的luci-app-frpc
-git clone  https://github.com/superzjg/luci-app-frpc_frps superzjg
-# rm -rf ./feeds/luci/applications/luci-app-frpc
-cp -r ./superzjg/luci-app-frpc ./package/
 
 echo '---当前执行步骤：custom_feeds自定义软件包-2-调整源码-2.1-通用源码-2.1.6-修改默认语言'
 sed -i "s|option lang auto|option lang \'zh_cn\'|g" ./feeds/luci/modules/luci-base/root/etc/config/luci
