69aba1e4b57d19ece867a7aa2ac9f1d40eed727e
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-09-23 22:49:28 +0800

    Update add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh

--- zhKong/scripts/add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh
+++ zhKong/scripts/add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh
@@ -7,4 +7,4 @@
 #echo dts文件内容为
 #cat ./target/linux/qualcommax/dts/ipq8072-sax1v1k.dts
 
-source ../$REPO_TYPE/scripts/add-sax1v1k-fan-hw.sh --channel2 
+source ../$REPO_TYPE/scripts/add-sax1v1k-fan-hw.sh --channel2 --proper
