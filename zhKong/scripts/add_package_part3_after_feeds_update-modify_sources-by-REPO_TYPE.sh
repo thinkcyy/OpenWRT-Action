dc319844ca6e981242168d784807ef8e37da47d1
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2026-09-23 20:05:54 +0800

    Update add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh

--- zhKong/scripts/add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh
+++ zhKong/scripts/add_package_part3_after_feeds_update-modify_sources-by-REPO_TYPE.sh
@@ -1,3 +1,8 @@
+echo 补丁config-6.12
+echo  CONFIG_PWM_IPQ=y >> target/linux/qualcommax/config-6.12
+echo config-6.12内容为
+cat target/linux/qualcommax/config-6.12
+
 cp -vr ../patch/target ./
 echo dts文件内容为
 cat ./target/linux/qualcommax/dts/ipq8072-sax1v1k.dts
